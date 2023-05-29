033733f7ecc9c7dc9d45af592679eea26f0ff33d
Caio Vitor <vitor.marchs@hotmail.com>
2023-05-29 20:19:28 -0300

    feat: criação das telas de cadastro e remoção de usuário.
    No último commit de mesmo título, eu havia esquecido de instanciar os arquivos, portanto o commit não tinha nada.

--- br/ufrn/imd/DAO/UsuariosDAO.java
+++ br/ufrn/imd/DAO/UsuariosDAO.java
@@ -23,15 +23,28 @@ public class UsuariosDAO {
         return usuariosDAO;
     }
 
-    public void addUsuario(Usuario usuario){
+    public boolean addUsuario(Usuario usuario){
+        for(int i = 0; i < listaUsuarios.size(); i++){
+            if(usuario.getNomeusuario().equals(listaUsuarios.get(i).getNomeusuario()) || usuario.getNomeusuario().equals("Admin")){
+                System.out.println("Usuário \u001B[33m" + usuario.getNomeusuario() + "\u001B[0m já foi cadastrado");
+                return false;
+            }
+        }
+        System.out.println("Usuário \u001B[36m" + usuario.getNomeusuario() + "\u001B[0m cadastrado com sucesso!");
         listaUsuarios.add(usuario);
-        System.out.println("Usuário: " + usuario.getNomeusuario() + " inserido com sucesso!");
-
+        return true;
     }
 
-    public void removeUsuario(Usuario usuario){
-        listaUsuarios.remove(usuario);
-        System.out.println("Usuário: " + usuario.getNomeusuario() + " removido com sucesso!");
+    public boolean removeUsuario(Usuario usuario){
+        for(int i = 0; i < listaUsuarios.size(); i++){
+            if(usuario.getNomeusuario().equals(listaUsuarios.get(i).getNomeusuario()) && !usuario.getNomeusuario().equals("Admin")) {
+                listaUsuarios.remove(listaUsuarios.get(i));
+                System.out.println("Usuário \u001B[31m" + usuario.getNomeusuario() + "\u001B[0m removido com sucesso!");
+                return true;
+            }
+        }
+        System.out.println("Usuário \u001B[33m" + usuario.getNomeusuario() + "\u001B[0m não encontrado");
+        return false;
     }
 
     public void listUsuarios(){
